45eba9aaa32ced98ee631fef0d15e58f1abeb36d
kasha-2 <karam.shamasyousif@gmail.com>
2020-04-12 16:39:56 -0500

    set toolbar, option menu

--- app/src/main/java/com/sasuke/covid19/MapsActivity.java
+++ app/src/main/java/com/sasuke/covid19/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.sasuke.covid19;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentActivity;
-
-import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,15 +20,29 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+	private static final String _IS_USE_SEEK_CHECKED = "menu_item_use_seek";
+
 	private GoogleMap mMap;
+
+	private SharedPreferences preferences;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
 
+		preferences = getPreferences(Context.MODE_PRIVATE);
+
 		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setNavigationIcon(R.drawable.ic_person_white_48dp);
 		setSupportActionBar(toolbar);
+
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// todo open user activity
+			}
+		});
 
 
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -32,6 +51,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 		mapFragment.getMapAsync(this);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_maps_toolbar, menu);
+
+		boolean isChecked = preferences.getBoolean(_IS_USE_SEEK_CHECKED, false);
+		menu.getItem(0).setChecked(isChecked);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_use_seek:
+				boolean check = item.isChecked();
+				item.setChecked(!check);
+
+				SharedPreferences.Editor editor = preferences.edit();
+				editor.putBoolean(_IS_USE_SEEK_CHECKED, !check).apply();
+				break;
+
+			case R.id.menu_about:
+				Intent intent = new Intent(this, AboutActivity.class);
+				startActivity(intent);
+				break;
+
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+
+		return true;
+	}
 
 	/**
 	 * Manipulates the map once available.
