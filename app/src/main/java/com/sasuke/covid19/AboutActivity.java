12d08ae15a4800eb7f05f3eddb90be55c39084a4
kasha-2 <karam.shamasyousif@gmail.com>
2020-05-14 22:06:07 -0500

    about activity verbiage

--- app/src/main/java/com/sasuke/covid19/AboutActivity.java
+++ app/src/main/java/com/sasuke/covid19/AboutActivity.java
@@ -1,8 +1,10 @@
 package com.sasuke.covid19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class AboutActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+		setSupportActionBar(toolbar);
+
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				onBackPressed();
+			}
+		});
 	}
 }
